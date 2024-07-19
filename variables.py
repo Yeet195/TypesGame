7abe04604c859bcd28ac8cf6d1ddff401405e671
Yeet195 <nils.kalinke11@gmail.com>
2024-07-19 10:53:57 +0200

    unified formatting

--- variables.py
+++ variables.py
@@ -1,21 +1,22 @@
-TYPES = ['Normal', 
-         'Fighting', 
-         'Flying', 
-         'Poison', 
-         'Ground', 
-         'Rock', 
-         'Bug', 
-         'Ghost', 
-         'Steel', 
-         'Fire', 
-         'Water', 
-         'Grass', 
-         'Electric', 
-         'Psychic', 
-         'Ice', 
-         'Dragon', 
-         'Dark', 
-         'Fairy']
+TYPES = [
+    'Normal', 
+    'Fighting', 
+    'Flying', 
+    'Poison', 
+    'Ground', 
+    'Rock', 
+    'Bug', 
+    'Ghost', 
+    'Steel', 
+    'Fire', 
+    'Water', 
+    'Grass', 
+    'Electric', 
+    'Psychic', 
+    'Ice', 
+    'Dragon', 
+    'Dark', 
+    'Fairy']
 
 WEAKNESSCHART = [
     [0, 2, 1],  # Normal
@@ -37,24 +38,25 @@ WEAKNESSCHART = [
     [2, 3, 0],  # dark
     [3, 3, 0]]  # fairy
 
-TYPECOLORS = ['#A8A77A', # normal
-              '#C22E28', # fighting
-              '#A98FF3', # flying
-              '#A33EA1', # poison
-              '#E2BF65', # ground
-              '#B6A136', #rock
-              '#A6B91A', # bug 
-              '#735797', # ghost
-              '#B7B7CE', # steel
-              '#EE8130', # fire
-              '#6390F0', # water
-              '#7AC74C', # grass
-              '#F7D02C', # electric
-              '#F95587', # psychic
-              '#96D9D6', # ice
-              '#6F35FC', # dragon
-              '#705746', # dark
-              '#D685AD'] # fairy
+TYPECOLORS = [
+    '#A8A77A', # normal
+    '#C22E28', # fighting
+    '#A98FF3', # flying
+    '#A33EA1', # poison
+    '#E2BF65', # ground
+    '#B6A136', #rock
+    '#A6B91A', # bug 
+    '#735797', # ghost
+    '#B7B7CE', # steel
+    '#EE8130', # fire
+    '#6390F0', # water
+    '#7AC74C', # grass
+    '#F7D02C', # electric
+    '#F95587', # psychic
+    '#96D9D6', # ice
+    '#6F35FC', # dragon
+    '#705746', # dark
+    '#D685AD'] # fairy
 
 FONT = "Fixedsys", 20
 BUTTON_BG = "#8178c7"
