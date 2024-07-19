d45feafb6cff9f4dbade5ff7e4a9a3016ef6ccdd
Yeet195 <nils.kalinke11@gmail.com>
2024-07-19 10:17:43 +0200

    major code overhaul

---
+++ vars.py
@@ -0,0 +1,64 @@
+TYPES = [
+    'Normal', 
+    'Fighting', 
+    'Flying', 
+    'Poison', 
+    'Ground', 
+    'Rock', 
+    'Bug', 
+    'Ghost', 
+    'Steel', 
+    'Fire', 
+    'Water', 
+    'Grass', 
+    'Electric', 
+    'Psychic', 
+    'Ice', 
+    'Dragon', 
+    'Dark', 
+    'Fairy']
+
+WEAKNESSCHART = [
+    [0, 2, 1],  # Normal
+    [5, 5, 1],  # fighting
+    [3, 3, 0],  # flying
+    [2, 4, 1],  # poison
+    [5, 2, 1],  # ground  
+    [4, 3, 0],  # rock
+    [3, 7, 0],  # bug
+    [2, 1, 1],  # ghost
+    [3, 4, 0],  # steel
+    [4, 4, 0],  # fire
+    [3, 3, 0],  # water
+    [3, 7, 0],  # grass
+    [2, 3, 0],  # electric
+    [2, 2, 0],  # psychic
+    [4, 4, 0],  # ice
+    [1, 1, 1],  # dragon
+    [2, 3, 0],  # dark
+    [3, 3, 0]]  # fairy
+
+TYPECOLORS = [
+    '#A8A77A', # normal
+    '#C22E28', # fighting
+    '#A98FF3', # flying
+    '#A33EA1', # poison
+    '#E2BF65', # ground
+    '#B6A136', #rock
+    '#A6B91A', # bug 
+    '#735797', # ghost
+    '#B7B7CE', # steel
+    '#EE8130', # fire
+    '#6390F0', # water
+    '#7AC74C', # grass
+    '#F7D02C', # electric
+    '#F95587', # psychic
+    '#96D9D6', # ice
+    '#6F35FC', # dragon
+    '#705746', # dark
+    '#D685AD'  # fairy
+    ] 
+
+FONT = "Fixedsys", 20
+BUTTON_BG = "#8178c7"
+buttons = {}
